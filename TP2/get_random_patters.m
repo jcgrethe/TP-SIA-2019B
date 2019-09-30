a8ce5bde29ba96c54045119ab54c811134ec029c
juan <jgrethe@itba.edu.ar>
2019-09-30 01:06:06 -0300

    load data

---
+++ TP2/get_random_patters.m
@@ -0,0 +1,14 @@
+function [input_patterns] = get_random_patters(dataset_path, patterns_percentage, normalize_function)
+  dataset = dlmread(dataset_path)(2:end, :);
+  pattern_qty = round(patterns_percentage * rows(dataset));
+  training_set = randperm(rows(dataset))(1:pattern_qty);  
+  for i = 1:pattern_qty
+    input_patterns(i, :) = dataset(training_set(i), 1:end);
+  endfor
+  % for i = 1:columns(input_patterns)
+  %   # https://datascience.stackexchange.com/questions/13178/how-to-normalize-data-for-neural-network-and-decision-forest
+  %   minX = min(input_patterns(:,i));
+  %   maxX = max(input_patterns(:,i));
+  %   input_patterns(:, i) = (input_patterns(:,i) - minX) / (maxX-minX) - 0.5;
+  % endfor
+endfunction
