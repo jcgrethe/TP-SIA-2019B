8e1910afccf7ff5e5b2f5bd1cd826911694fef09
Martin <martinnagelberg@gmail.com>
2019-10-01 01:41:02 -0300

    fix + cont. incremental trainer

---
+++ TP2/get_random_patterns.m
@@ -0,0 +1,13 @@
+function [input_patterns] = get_random_patterns(dataset_path, patterns_percentage)
+  dataset = dlmread(dataset_path)(2:end, :);
+  pattern_qty = round( (patterns_percentage / 100) * rows(dataset));
+  training_set = randperm(rows(dataset))(1:pattern_qty);  
+  for i = 1:pattern_qty
+    input_patterns(i, :) = dataset(training_set(i), 1:end);
+  endfor
+   for i = 1:columns(input_patterns)
+     minX = min(input_patterns(:,i));
+     maxX = max(input_patterns(:,i));
+     input_patterns(:, i) = (input_patterns(:,i) - minX) / (maxX-minX);
+   endfor
+endfunction
