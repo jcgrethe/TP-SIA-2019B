96441386802a2810d7e42f787ec292a66c6b0cfd
juan <jgrethe@itba.edu.ar>
2019-10-07 10:37:47 -0300

    w0

--- TP2/data_init.m
+++ TP2/data_init.m
@@ -1,5 +1,8 @@
 # Learning data
 input_patterns = get_random_patterns(total_patterns, LEARNING_PERCENTAGE);
+if(size(w0) != 0)
+    input_patterns = [input_patterns; w0];
+endif
 figure(1);
 plot_terrain(total_patterns);
 
